7f59d456fc8caf3f56d3a904e2c433355e9d9cf6
FuJiGraphics <chlcjfwls1@naver.com>
2024-01-26 17:36:19 +0900

    Deleted a Pylon Sample Files.

--- Vendor/Pylon/Samples/C/buildall.cmd
+++
@@ -1,4 +0,0 @@
-REM loop over all directories and call make for each of them
-REM aborts if any make fails and returns an error
-
-for /D %%d in (*) do pushd %%d% & make || (popd & exit /B 1) & popd
